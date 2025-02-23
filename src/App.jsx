5beb8e0c12930d3ee1ad3f28d70bec83a819cc8f
oscar <oscar.burman005@gmail.com>
2025-02-23 19:55:38 +0100

    Installed react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-t from-gray-600 to-gray-900">
+      <h1 className="text-5xl text-white font-semibold">React + Tailwind css</h1>
+    </div>
+  );
+}
+
+export default App;
